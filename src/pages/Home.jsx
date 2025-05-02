f9cef685c499423a43a4ff850a10a826ecb819da
sipon <fakhrulislamsipon@gmail.com>
2025-05-02 11:19:04 +0600

    scroll behaviar implemanted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from "react-router";
 
 const Home = () => {
-  return <Navigate to="/category/1"></Navigate>;
+  return <Navigate to="/category/0"></Navigate>;
 };
 
 export default Home;
